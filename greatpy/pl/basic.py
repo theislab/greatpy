c0a11144bdaf1cd2e7cbcfe1eaeb4cfdde7d90ff
Tom MAUGER-BIROCHEAU <tom.mauger-birocheau@etu.univ-cotedazur.fr>
2022-06-27 14:05:11 +0200

    Modify the syntax

--- greatpy/pl/basic.py
+++ greatpy/pl/basic.py
@@ -8,12 +8,12 @@ def basic_plot(adata: AnnData) -> int:
     print("Import matplotlib and implement a plotting function here.")
     return 0
 
-def scatterplot(Great_df:pd.DataFrame,colname_x,colname_y,minus_log10=True):
-    Great_df = Great_df.dropna()
-    Great_df = Great_df.astype({colname_x:"float64",colname_y:"float64"})
+def scatterplot(great_df:pd.DataFrame,colname_x,colname_y,minus_log10=True):
+    great_df = great_df.dropna()
+    great_df = great_df.astype({colname_x:"float64",colname_y:"float64"})
     if minus_log10 :
-        Great_df[f"-log({colname_x})"] = -log(Great_df[colname_x])
-        Great_df[f"-log({colname_y})"] = -log(Great_df[colname_y])
-        sp(data=Great_df,x=f"-log({colname_x})",y=f"-log({colname_y})")
+        great_df[f"-log({colname_x})"] = -log(great_df[colname_x])
+        great_df[f"-log({colname_y})"] = -log(great_df[colname_y])
+        sp(data=great_df,x=f"-log({colname_x})",y=f"-log({colname_y})")
     else : 
-        sp(data=Great_df,x=colname_x,y=colname_y)
+        sp(data=great_df,x=colname_x,y=colname_y)
