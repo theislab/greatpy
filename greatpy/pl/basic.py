15c382c8bd119c6c22b570a718d48025b2aca836
TomMB45 <tom.mauger-birocheau@etu.univ-cotedazur.fr>
2022-07-08 16:20:02 +0200

    Add title to the plot

--- greatpy/pl/basic.py
+++ greatpy/pl/basic.py
@@ -9,13 +9,13 @@ def basic_plot(adata: AnnData) -> int:
     print("Import matplotlib and implement a plotting function here.")
     return 0
 
-def scatterplot(great_df:pd.DataFrame,colname_x,colname_y,minus_log10=True):
+def scatterplot(great_df:pd.DataFrame,colname_x,colname_y,title:str="",minus_log10=True):
     great_df = great_df.dropna()
     great_df = great_df.astype({colname_x:"float64",colname_y:"float64"})
     if minus_log10 :
         great_df[f"-log({colname_x})"] = -log(great_df[colname_x])
         great_df[f"-log({colname_y})"] = -log(great_df[colname_y])
-        sp(data=great_df,x=f"-log({colname_x})",y=f"-log({colname_y})")
+        sp(data=great_df,x=f"-log({colname_x})",y=f"-log({colname_y})").set_title(title)
     else : 
-        sp(data=great_df,x=colname_x,y=colname_y)
+        sp(data=great_df,x=colname_x,y=colname_y).set_title(title)
     plt.show()
