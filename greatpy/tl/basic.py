9107e808e2462f579c6f8e52c54741d4752946ad
TomMB45 <tom.mauger-birocheau@etu.univ-cotedazur.fr>
2022-07-08 09:27:36 +0200

    Add docstrings

--- greatpy/tl/basic.py
+++ greatpy/tl/basic.py
@@ -1,3 +1,4 @@
+from types import NoneType
 from anndata import AnnData
 import pandas as pd
 pd.options.display.float_format = '{:12.5e}'.format
@@ -8,32 +9,34 @@ def basic_tool(adata: AnnData) -> int:
     print("Implement a tool to run on the AnnData object.")
     return 0
 
-
-max_extension = 1000000  
-basal_upstream = 5000
-basal_downstream = 1000
-
-def file_reader(path:str,type_f:str):
+def validate_input(association:str,max_extension:int,basal_upstream:int,basal_downstream:int): 
     """
-    path of the file and type of data in the file 
+    This function checks that the inputs (association_rule, max_extension, basal_upstream, basal_downstream) are valid 
 
-    type should be : 
-        tss if : "Chr","tss","strand","name"
-        chr_size if : "Chr","Chr_Size" 
+    Parameters
+    ----------
+    association : str 
+        The association rule to use.
+    max_extension : int
+        The maximum extension of the regulatory domain.
+    basal_upstream : int
+        The basal upstream of the regulatory domain.
+    basal_downstream : int
+        The basal downstream of the regulatory domain.
+
+    Returns
+    -------
+    bool
+        True if the inputs are valid, False otherwise.
+
+    Exemples 
+    --------
+    >>> validate_input("Two_Closet")
+        True
+
+    >>> validate_input("Two_Closet_Extension")
+        False
     """
-    if type_f not in ["tss","chr_size"]: 
-        print("type should be tss or chr_size")
-        return False
-
-    elif type_f == "tss": 
-        data=pd.read_csv(path,sep="\t",comment="#",names=["Chr","tss","Strand","name"])
-        return data
-
-    elif type_f == "chr_size": 
-        data=pd.read_csv(path,sep="\t",comment="#",names=["Chr","Size"])
-        return data
-
-def validate_input(association): 
     if association != "OneCloset" and association != "TwoCloset" and association != "Basalplusextention" : 
         print("Association rule should be OneCloset or TwoCloset Basalplusextention")
         return False
@@ -48,7 +51,21 @@ def validate_input(association):
         return False
     return True
 
-def write_Regdom(regdom:pd.DataFrame,file_name):
+def write_Regdom(regdom:pd.DataFrame,file_name:str):
+    """
+    This method allows you to write the regulatory regions calculated in a file given in argument
+
+    Parameters
+    ----------
+    regdom : pd.DataFrame 
+        The regulatory regions to write.
+    file_name : str
+        The path of the file to write.
+
+    Returns
+    -------
+    None.
+    """
     f = open(file_name,"w")
     f.write("#chr\tChrStart\tChrEnd\tname\ttss\tstrand\n")
     for i in range(regdom.shape[0]): 
@@ -57,13 +74,53 @@ def write_Regdom(regdom:pd.DataFrame,file_name):
         f.write(f"{chr}\t{start}\t{end}\t{name}\t{tss}\t{strand}\n")
     f.close()
 
-def create_basal_plus_extension_regdom(regdom:pd.DataFrame,maximumExtension,basalUp,basalDown,chr_size):
+def create_basal_plus_extension_regdom(tss:pd.DataFrame,maximumExtension:int,basalUp:int,basalDown:int,chr_size:pd.DataFrame):
+    """
+    This function allows to create the regulatory domains using the Basalplusextention association rule
+
+    Parameters
+    ----------
+    tss : pd.DataFrame
+        The TSS of the genes.
+    maximumExtension : int
+        The maximum extension of the regulatory domain.
+    basalUp : int
+        The basal upstream of the regulatory domain.
+    basalDown : int
+        The basal downstream of the regulatory domain.
+    chr_size : pd.DataFrame
+        The chromosome size.
+
+    Returns
+    -------
+    tss : pd.DataFrame
+        The regulatory domains.
+
+    Exemples 
+    --------
+    >>> regdom = create_basal_plus_extension_regdom(
+    ...    tss_file=pd.read_csv("../../data/human/tss.bed",sep="\t",names=["Chr","tss","Strand"]),
+    ...    maximumExtension=100000,
+    ...    basalUp=5000,
+    ...    basalDown=1000,
+    ...    chr_sizes=pd.read_csv("../../data/human/chr_size.bed",sep="\t",names=["Chr","Size"])
+    ... )
+    >>> regdom.head()
+    ...    |    | Chr   |   Chr_Start |   Chr_End | name      |   tss | Strand   |
+    ...    |---:|:------|------------:|----------:|:----------|------:|:---------|
+    ...    |  0 | chr1  |           0 |     22436 | MIR6859-1 | 17436 | -        |
+    ...    |  1 | chr1  |       16436 |     22436 | MIR6859-2 | 17436 | -        |
+    ...    |  2 | chr1  |       16436 |     22436 | MIR6859-3 | 17436 | -        |
+    ...    |  3 | chr1  |       16436 |     28370 | MIR6859-4 | 17436 | -        |
+    ...    |  4 | chr1  |       22436 |     34370 | WASH7P    | 29370 | -        |
+    
+    """
     prev = curr = next = 0
     chr_strat_end = []
     start = []
     end = []
-    for i in range(regdom.shape[0]):
-        curr = regdom.iloc[i]
+    for i in range(tss.shape[0]):
+        curr = tss.iloc[i]
         chr = curr["Chr"]
         curr_chr_size = int(chr_size.loc[chr_size["Chr"]==chr,"Size"])
         tmp = curr["tss"]
@@ -82,13 +139,13 @@ def create_basal_plus_extension_regdom(regdom:pd.DataFrame,maximumExtension,basa
             return False
         chr_strat_end.append([curr_chr_start,curr_chr_end])
 
-    for i in range(regdom.shape[0]):
-        curr = regdom.iloc[i]
+    for i in range(tss.shape[0]):
+        curr = tss.iloc[i]
         chr = curr["Chr"]
         
         curr_chr_size = int(chr_size.loc[chr_size["Chr"]==chr,"Size"])
-        if i != regdom.shape[0]-1 : 
-            next = regdom.iloc[i+1]
+        if i != tss.shape[0]-1 : 
+            next = tss.iloc[i+1]
         else : next = 0
 
 
@@ -103,7 +160,7 @@ def create_basal_plus_extension_regdom(regdom:pd.DataFrame,maximumExtension,basa
             tmp_start = min(basal_start,max(prev_end,tmp_start))
 
 
-        tmp_end = min(curr_chr_size,curr["tss"]+max_extension)
+        tmp_end = min(curr_chr_size,curr["tss"]+1000000)
         basal_end = chr_strat_end[i][1]
         
         tmp_end = max(basal_end,tmp_end)
@@ -114,26 +171,99 @@ def create_basal_plus_extension_regdom(regdom:pd.DataFrame,maximumExtension,basa
                 nextStart = next["tss"]-basalDown
             tmp_end = max(basal_end,min(nextStart,tmp_end))
             
-        prev = regdom.iloc[i]
+        prev = tss.iloc[i]
         start.append(int(tmp_start))
         end.append(int(tmp_end))
-    regdom["Chr_Start"] = start
-    regdom["Chr_End"] = end
-    return regdom
+    tss["Chr_Start"] = start
+    tss["Chr_End"] = end
+    return tss
 
-def create_Two_Closet_Regdom(regdom,max_extension,chr_size):
-    return create_basal_plus_extension_regdom(regdom,max_extension,0,0,chr_size)
+def create_Two_Closet_Regdom(tss:pd.DataFrame,max_extension:int,chr_size:pd.DataFrame):
+    """
+    This function allows to create the regulatory domains using the TwoCloset association rule. 
+    It is based on the basal plus extension rule but with basalUp and basalDown equals to 0.
 
-def create_one_closet_regdom(regdom:pd.DataFrame,maximum_extension,chr_size:pd.DataFrame):
+    Parameters
+    ----------
+    tss : pd.DataFrame
+        The TSS of the genes.
+    maximumExtension : int
+        The maximum extension of the regulatory domain.
+    chr_size : pd.DataFrame
+        The chromosome size.
+
+    Returns
+    -------
+    pd.DataFrame
+        The regulatory domains.
+
+    Exemples 
+    --------
+    >>> regdom = create_Two_Closet_regdom(
+    ...    tss_file=pd.read_csv("../../data/human/tss.bed",sep="\t",names=["Chr","tss","Strand"]),
+    ...    maximumExtension=100000,
+    ...    chr_sizes=pd.read_csv("../../data/human/chr_size.bed",sep="\t",names=["Chr","Size"])
+    ... )
+    >>> regdom.head()
+    ...    |    | Chr   |   Chr_Start |   Chr_End | name      |   tss | Strand   |
+    ...    |---:|:------|------------:|----------:|:----------|------:|:---------|
+    ...    |  0 | chr1  |           0 |     17436 | MIR6859-1 | 17436 | -        |
+    ...    |  1 | chr1  |       17436 |     17436 | MIR6859-2 | 17436 | -        |
+    ...    |  2 | chr1  |       17436 |     17436 | MIR6859-3 | 17436 | -        |
+    ...    |  3 | chr1  |       17436 |     29370 | MIR6859-4 | 17436 | -        |
+    ...    |  4 | chr1  |       17436 |     30365 | WASH7P    | 29370 | -        |
+    
+    """
+    return create_basal_plus_extension_regdom(tss,max_extension,0,0,chr_size)
+
+def create_one_closet_regdom(tss:pd.DataFrame,maximum_extension:int,chr_size:pd.DataFrame):
+    """
+    This function allows to create the regulatory domains using the OneCloset association rule
+
+    Parameters
+    ----------
+    tss : pd.DataFrame
+        The TSS of the genes.
+    maximumExtension : int
+        The maximum extension of the regulatory domain.
+    basalUp : int
+        The basal upstream of the regulatory domain.
+    basalDown : int
+        The basal downstream of the regulatory domain.
+    chr_size : pd.DataFrame
+        The chromosome size.
+
+    Returns
+    -------
+    tss : pd.DataFrame
+        The regulatory domains.
+
+    Exemples 
+    --------
+    >>> regdom = create_basal_plus_extension_regdom(
+    ...    tss_file=pd.read_csv("../../data/human/tss.bed",sep="\t",names=["Chr","tss","Strand"]),
+    ...    maximum_extension=100000,
+    ...    chr_sizes=pd.read_csv("../../data/human/chr_size.bed",sep="\t",names=["Chr","Size"])
+    ... )
+    >>> regdom.head()
+    ...    |    | Chr   |   Chr_Start |   Chr_End | name      |   tss | Strand   |
+    ...    |---:|:------|------------:|----------:|:----------|------:|:---------|
+    ...    |  0 | chr1  |           0 |     17436 | MIR6859-1 | 17436 | -        |
+    ...    |  1 | chr1  |       17436 |     17436 | MIR6859-2 | 17436 | -        |
+    ...    |  2 | chr1  |       17436 |     17436 | MIR6859-3 | 17436 | -        |
+    ...    |  3 | chr1  |       17436 |     23403 | MIR6859-4 | 17436 | -        |
+    ...    |  4 | chr1  |       23403 |     29867 | WASH7P    | 29370 | -        |
+    
+    """
     prev = curr = next = 0
     start = []
     end = []
 
-    for i in range(regdom.shape[0]):
-        curr = regdom.iloc[i]
+    for i in range(tss.shape[0]):
+        curr = tss.iloc[i]
         chr = curr["Chr"]
         curr_chr_size = int(chr_size.loc[chr_size["Chr"]==chr,"Size"])
-        if i < regdom.shape[0]-1 : next = regdom.iloc[i+1]
+        if i < tss.shape[0]-1 : next = tss.iloc[i+1]
         else : next = 0
 
         if (type(prev) == int and prev == 0) or (type(prev) != int and prev["Chr"] != curr["Chr"]) : prev = 0 
@@ -153,19 +283,61 @@ def create_one_closet_regdom(regdom:pd.DataFrame,maximum_extension,chr_size:pd.D
         end.append(tmp_end)
         prev = curr
         
-    regdom["Chr_Start"] = start
-    regdom["Chr_End"] = end
-    return regdom
+    tss["Chr_Start"] = start
+    tss["Chr_End"] = end
+    return tss
 
-def create_regdom(tss_file,chr_sizes_file,association_rule,out_path): 
+def create_regdom(tss_file,chr_sizes_file,association_rule,max_extension:int=1000000,basal_upstream:int=5000,basal_downstream:int=1000, out_path:str or NoneType=None): 
+    """
+    This function allows to create regdoms according to the three association rules, to write the result in a file or not and to return the result as a pd.DataFrame
+
+    Parameters
+    ----------
+    tss_file : str
+        The path of the TSS file.
+    chr_sizes_file : str
+        The path of the chromosome size file.
+    association_rule : str
+        The association rule to use. Could be : "OneCloset", "TwoCloset", "BasalPlusExtension".
+    maximumExtension : int : default 1000000
+        The maximum extension of the regulatory domain.
+    basalUp : int : default 5000
+        The basal upstream of the regulatory domain.
+    basalDown : int : default 1000
+        The basal downstream of the regulatory domain.
+    out_path : str or NoneType : default None
+        The path of the output file.
+
+    Returns
+    -------
+    out : pd.DataFrame
+        The regulatory domains.
+
+    Exemples 
+    --------
+    >>> regdom = create_regdom(
+    ...    tss_file="../../data/human/tss.bed",
+    ...    chr_sizes_file="../../data/human/chr_size.bed",sep="\t",names=["Chr","Size"],
+    ...    association_rule="OneCloset",
+    ... )
+    >>> regdom.head()
+    ...    |    | Chr   |   Chr_Start |   Chr_End | name      |   tss | Strand   |
+    ...    |---:|:------|------------:|----------:|:----------|------:|:---------|
+    ...    |  0 | chr1  |           0 |     17436 | MIR6859-1 | 17436 | -        |
+    ...    |  1 | chr1  |       17436 |     17436 | MIR6859-2 | 17436 | -        |
+    ...    |  2 | chr1  |       17436 |     17436 | MIR6859-3 | 17436 | -        |
+    ...    |  3 | chr1  |       17436 |     23403 | MIR6859-4 | 17436 | -        |
+    ...    |  4 | chr1  |       23403 |     29867 | WASH7P    | 29370 | -        |
+
+    """
     if not validate_input(association_rule): 
         print("Invalid input")
         return False
-    df = file_reader(tss_file,'tss')
+    df = pd.read_csv(tss_file,sep="\t",comment="#",names=["Chr","tss","Strand","name"])
     
     df = df.sort_values(["Chr","tss","Strand","name"])
 
-    chr_size = file_reader(chr_sizes_file,"chr_size") 
+    chr_size = pd.read_csv(chr_sizes_file,sep="\t",comment="#",names=["Chr","Size"])
 
     if association_rule == "OneCloset" : 
         out = create_one_closet_regdom(df,max_extension,chr_size)
