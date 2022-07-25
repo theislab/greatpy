15de62a8cc1f2d05a10adf52a7b8356a7f137fae
TomMB45 <tom.mauger-birocheau@etu.univ-cotedazur.fr>
2022-07-25 09:14:18 +0200

    add dotplot function

--- greatpy/pl/basic.py
+++ greatpy/pl/basic.py
@@ -1,7 +1,11 @@
 import pandas as pd 
 from numpy import log
-from seaborn import scatterplot as sp,barplot as bar 
 import greatpy as gp
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+from matplotlib import colors
+from matplotlib import rcParams
 
 def scatterplot(great_df:pd.DataFrame,colname_x:str,colname_y:str,title:str="",minus_log10=True,ax=None) -> None :
     """
@@ -38,9 +42,9 @@ def scatterplot(great_df:pd.DataFrame,colname_x:str,colname_y:str,title:str="",m
     if minus_log10 :
         great_df[f"-log({colname_x})"] = -log(great_df[colname_x])
         great_df[f"-log({colname_y})"] = -log(great_df[colname_y])
-        sp(data=great_df,x = f"-log({colname_x})",y = f"-log({colname_y})",ax = ax).set_title(title)
+        sb.scatterplot(data=great_df,x = f"-log({colname_x})",y = f"-log({colname_y})",ax = ax).set_title(title)
     else : 
-        sp(data=great_df,x=colname_x,y=colname_y,ax=ax).set_title(title)
+        sb.scatterplot(data=great_df,x=colname_x,y=colname_y,ax=ax).set_title(title)
 
 
 def graph_nb_asso_per_peaks(test:str or pd.DataFrame,regdom:str or pd.DataFrame,ax=None,color=None) -> None :
@@ -68,9 +72,9 @@ def graph_nb_asso_per_peaks(test:str or pd.DataFrame,regdom:str or pd.DataFrame,
     nb_asso_per_peaks = gp.tl.get_nb_asso_per_region(test,regdom)
 
     nb = {
-        "number" : [],
-        "number_genes" : [],
-        "percentage" : [],
+     "number" : [],
+     "number_genes" : [],
+     "percentage" : [],
     }
     for i in list(set(nb_asso_per_peaks.values())) :
         nb["number"].append(i)
@@ -78,7 +82,7 @@ def graph_nb_asso_per_peaks(test:str or pd.DataFrame,regdom:str or pd.DataFrame,
         nb["percentage"].append(round(( list(nb_asso_per_peaks.values()).count(i) / len(nb_asso_per_peaks.keys()) ) * 100))
     nb = pd.DataFrame(nb,columns=["number","number_genes","percentage"],index=nb["number"])
 
-    g = bar(data = nb,x="number",y="percentage",ax=ax,color=color)
+    g = sb.barplot(data = nb,x="number",y="percentage",ax=ax,color=color)
     g.set_title("Number of associated genes per region")
     g.set_xlabel("Number of associated genes per region")
     g.set_ylabel("Genomic region (%)")
@@ -137,7 +141,7 @@ def graph_dist_tss(test:str or pd.DataFrame,regdom:str or pd.DataFrame,ax=None,c
     df = pd.DataFrame(res).transpose().rename(columns={0 : "count"})
     df["percentage"] = (df["count"] / nb) * 100
     df = df.reset_index(drop=False).rename(columns={"index" : "distance"})
-    g = bar(data=df,x="distance",y="percentage",color=color,ax=ax)
+    g = sb.barplot(data=df,x="distance",y="percentage",color=color,ax=ax)
     for idx,p in enumerate (g.patches) : 
         g.annotate(str(df.iloc[idx]["count"]),(p.get_x() + p.get_width() / 2 , p.get_height()))
     g.set_xlabel("Distance to TSS (kb)")
@@ -186,9 +190,114 @@ def graph_absolute_dist_tss(test:str or pd.DataFrame,regdom:str or pd.DataFrame,
     df = pd.DataFrame(res).transpose().rename(columns={0:"count"})
     df["percentage"] = (df["count"]/nb)*100
     df = df.reset_index(drop=False).rename(columns={"index":"distance"})
-    g = bar(data=df,x="distance",y="percentage",color=color,ax=ax)
+    g = sb.barplot(data=df,x="distance",y="percentage",color=color,ax=ax)
     for idx,p in enumerate (g.patches) : 
         g.annotate(str(df.iloc[idx]["count"]),(p.get_x() + p.get_width() / 2 , p.get_height()))
     g.set_xlabel("Absolute distance to TSS (kb)")
     g.set_ylabel("Genomic region (%)")
     g.set_title("Binned by absolute distance to TSS")
+
+
+def scale_data_5_75(data):
+    mind = np.min(data)
+    maxd = np.max(data)
+    
+    if maxd == mind:
+        maxd=maxd+1
+        mind=mind-1
+        
+    drange = maxd - mind
+    return ((((data - mind)/drange*0.70)+0.05)*100)
+
+
+def plot_enrich(data, n_terms=20, save=False):
+    # Test data input
+    if not isinstance(data, pd.DataFrame):
+        raise ValueError('Please input a Pandas Dataframe output by gprofiler.')
+        
+    if not np.all([term in data.columns for term in ['p_value', 'name', 'intersection_size']]):
+        raise TypeError('The data frame {} does not contain enrichment results from gprofiler.'.format(data))
+    
+    data_to_plot = data.iloc[:n_terms,:].copy()
+    data_to_plot['go.id'] = data_to_plot.index
+
+    min_pval = data_to_plot['p_value'].min()
+    max_pval = data_to_plot['p_value'].max()
+    
+    # Scale intersection_size to be between 5 and 75 for plotting
+    #Note: this is done as calibration was done for values between 5 and 75
+    data_to_plot['scaled.overlap'] = scale_data_5_75(data_to_plot['intersection_size'])
+    
+    norm = colors.LogNorm(min_pval, max_pval)
+    sm = plt.cm.ScalarMappable(cmap="cool", norm=norm)
+    sm.set_array([])
+
+    rcParams.update({'font.size': 14, 'font.weight': 'bold'})
+
+    sb.set(style="whitegrid")
+
+    path = plt.scatter(x='recall', y="name", c='p_value', cmap='cool', 
+                    norm=colors.LogNorm(min_pval, max_pval), 
+                    data=data_to_plot, linewidth=1, edgecolor="grey", 
+                    s=[(i+10)**1.5 for i in data_to_plot['scaled.overlap']])
+    ax = plt.gca()
+    ax.invert_yaxis()
+
+    ax.set_ylabel('')
+    ax.set_xlabel('Gene ratio', fontsize=14, fontweight='bold')
+    ax.xaxis.grid(False)
+    ax.yaxis.grid(True)
+
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # Get tick marks for this plot
+    #Note: 6 ticks maximum
+    min_tick = np.floor(np.log10(min_pval)).astype(int)
+    max_tick = np.ceil(np.log10(max_pval)).astype(int)
+    tick_step = np.ceil((max_tick - min_tick)/6).astype(int)
+    
+    # Ensure no 0 values
+    if tick_step == 0:
+        tick_step = 1
+        min_tick = max_tick-1
+    
+    ticks_vals = [10**i for i in range(max_tick, min_tick-1, -tick_step)]
+    ticks_labs = ['$10^{'+str(i)+'}$' for i in range(max_tick, min_tick-1, -tick_step)]
+
+    #Colorbar
+    fig = plt.gcf()
+    cbaxes = fig.add_axes([0.8, 0.15, 0.03, 0.4])
+    cbar = ax.figure.colorbar(sm, ticks=ticks_vals, shrink=0.5, anchor=(0,0.1), cax=cbaxes)
+    cbar.ax.set_yticklabels(ticks_labs)
+    cbar.set_label("Adjusted p-value", fontsize=14, fontweight='bold')
+
+    #Size legend
+    min_olap = data_to_plot['intersection_size'].min()
+    max_olap = data_to_plot['intersection_size'].max()
+    olap_range = max_olap - min_olap
+    
+    #Note: approximate scaled 5, 25, 50, 75 values are calculated
+    #      and then rounded to nearest number divisible by 5
+    size_leg_vals = [np.ceil(i/5)*5 for i in 
+                        [min_olap, min_olap+(20/70)*olap_range, min_olap+(45/70)*olap_range, max_olap]]
+    size_leg_scaled_vals = scale_data_5_75(size_leg_vals)
+
+    
+    l1 = plt.scatter([],[], s=(size_leg_scaled_vals[0]+10)**1.5, edgecolors='none', color='black')
+    l2 = plt.scatter([],[], s=(size_leg_scaled_vals[1]+10)**1.5, edgecolors='none', color='black')
+    l3 = plt.scatter([],[], s=(size_leg_scaled_vals[2]+10)**1.5, edgecolors='none', color='black')
+    l4 = plt.scatter([],[], s=(size_leg_scaled_vals[3]+10)**1.5, edgecolors='none', color='black')
+
+    labels = [str(int(i)) for i in size_leg_vals]
+
+    leg = plt.legend([l1, l2, l3, l4], labels, ncol=1, frameon=False, fontsize=12,
+                    handlelength=1, loc = 'center left', borderpad = 1, labelspacing = 1.4,
+                    handletextpad=2, title='Gene overlap', scatterpoints = 1,  bbox_to_anchor=(-2, 1.5), 
+                    facecolor='black')
+
+    if save:
+        plt.savefig(save, dpi=300, format='pdf', bbox_inches='tight')
+
+    plt.show()
