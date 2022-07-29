12b9e08e804b1a6acabb595342833f247c91abda
Tom MAUGER-BIROCHEAU <tom.mauger-birocheau@etu.univ-cotedazur.fr>
2022-07-29 16:24:50 +0200

    modify the html theme

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "scanpydoc"
+html_theme = "pyramid"
 html_static_path = ["_static"]
 
 pygments_style = "sphinx"
